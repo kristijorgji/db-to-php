e80d10665ff67872423986eb037e646f4cb3b7ea
Kristi Jorgji <15630854+kristijorgji@users.noreply.github.com>
2026-08-28 13:26:50 +0000

    Fix integration tests: env-driven default config + spatial fixtures
    - dbToPhp.cfg.php connection now reads DB_* env vars (localhost defaults) instead of a hardcoded private IP, so default-config integration tests reach the seeded DB.
    - Add missing SpecialEntity/SpecialEntityFactory expected fixtures for the 'special' (spatial-types) table added in bc1917a.
    
    Co-authored-by: Kristi Jorgji <kristijorgji@users.noreply.github.com>

---
+++ tests/integration/MySql/Php/output/factories/expected/SpecialEntityFactory.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Factories\Entities;
+
+use kristijorgji\DbToPhp\Data\AbstractEntityFactory;
+use Entities\SpecialEntity;
+
+class SpecialEntityFactory extends AbstractEntityFactory
+{
+    /**
+     * @var array
+     */
+    protected static $fields = [
+        'geometry',
+        'geometry_collection',
+        'json',
+        'line_string',
+        'multilinestring',
+        'point',
+        'multipoint',
+        'polygon',
+        'multy_polygon',
+    ];
+
+    /**
+     * @param array $data
+     * @return SpecialEntity
+     */
+    public static function make(array $data = []): SpecialEntity
+    {
+        return self::makeFromData(self::makeData($data));
+    }
+
+    /**
+     * @param array $data
+     * @return SpecialEntity
+     */
+    public static function makeFromData(array $data): SpecialEntity
+    {
+        self::validateData($data);
+        return self::mapArrayToEntity($data, SpecialEntity::class);
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    public static function makeData(array $data = []): array
+    {
+        self::validateData($data);
+        return [
+            'geometry' => array_key_exists('geometry', $data) ?
+                $data['geometry'] : self::randomString(rand(0, 64)),
+            'geometry_collection' => array_key_exists('geometry_collection', $data) ?
+                $data['geometry_collection'] : self::randomString(rand(0, 64)),
+            'json' => array_key_exists('json', $data) ?
+                $data['json'] : self::randomString(rand(0, 64)),
+            'line_string' => array_key_exists('line_string', $data) ?
+                $data['line_string'] : self::randomString(rand(0, 64)),
+            'multilinestring' => array_key_exists('multilinestring', $data) ?
+                $data['multilinestring'] : self::randomString(rand(0, 64)),
+            'point' => array_key_exists('point', $data) ?
+                $data['point'] : self::randomString(rand(0, 64)),
+            'multipoint' => array_key_exists('multipoint', $data) ?
+                $data['multipoint'] : self::randomString(rand(0, 64)),
+            'polygon' => array_key_exists('polygon', $data) ?
+                $data['polygon'] : self::randomString(rand(0, 64)),
+            'multy_polygon' => array_key_exists('multy_polygon', $data) ?
+                $data['multy_polygon'] : self::randomString(rand(0, 64)),
+        ];
+    }
+}
